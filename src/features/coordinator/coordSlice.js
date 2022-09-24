b5dcf11fca8eafe54b3679801c5582243d19921e
Ovikx <ovikdas10@gmail.com>
2022-09-24 00:59:52 -0500

    added coordinator boolean redux state

---
+++ src/features/coordinator/coordSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const coordSlice = createSlice({
+    name: 'coord',
+    initialState: {
+      value: false,
+    },
+    reducers: {
+      toggle: (state) => {
+        // Redux Toolkit allows us to write "mutating" logic in reducers. It
+        // doesn't actually mutate the state because it uses the Immer library,
+        // which detects changes to a "draft state" and produces a brand new
+        // immutable state based off those changes
+        state.value = !state.value;
+      },
+
+      login: (state) => {
+        state.value = true;
+      },
+
+      logout: (state) => {
+        state.value = false;
+      }
+    },
+  })
+  
+  // Action creators are generated for each case reducer function
+  export const { toggle, login, logout } = coordSlice.actions;
+  
+  export default coordSlice.reducer;
