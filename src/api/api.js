b2f806285adde6087ed1c10bc78935a07feb9cb7
AhmedA1559 <95593064+AhmedA1559@users.noreply.github.com>
2022-09-24 23:23:09 -0500

    API stuff

---
+++ src/api/api.js
@@ -0,0 +1,56 @@
+// convert
+const instance = axios.create({
+    baseURL: 'https://api.example.com'
+});
+
+function setAuth(authToken) {
+    instance.defaults.headers.common['Authorization'] = authToken;
+}
+
+function getEventAttendance(eventId) {
+    return instance.get(`/attendance/${eventId}`);
+}
+
+function getLeaderboardByCollege(collegeName) {
+    return instance.get(`/colleges/${collegeName}/leaderboard/`);
+}
+
+function getUserById(userId) {
+    return instance.get(`/users/${userId}`);
+}
+
+function getEventsByCollege(collegeName, eventId) {
+    return instance.get(`/events/${collegeName}`);
+}
+
+function getCollaborators(collegeName) {
+    return instance.get(`/collaborators/${collegeName}`);
+}
+
+function createEvent(collegeName, event) {
+    return instance.post(`/events/${collegeName}`, event);
+}
+
+function updateEvent(collegeName, eventId, event) {
+    return instance.put(`/events/${collegeName}`, event, { params: {event_id: eventId}});
+}
+
+function deleteEvent(collegeName, eventId) {
+    return instance.delete(`/events/${collegeName}/${eventId}`);
+}
+
+function addAttendance(eventId) {
+    return instance.post(`/attendance/${eventId}`);
+}
+
+function removeAttendance(eventId) {
+    return instance.delete(`/attendance/${eventId}`);
+}
+
+function updateUserInfo(userId, collegeName) {
+    return instance.put(`/users/${userId}`, { 'affiliation': collegeName});
+}
+
+function updateAttendance(eventId, attendance) {
+    return instance.put(`/attendance/${eventId}`);
+}
